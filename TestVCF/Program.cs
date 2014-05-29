7aa795a9baf5a0031b3a097d79cf04cde68d3c7c
Nigel Delaney <ndelaney@broadinstitute.org>
2014-05-29 12:11:20 -0400

    Add test data and example file.

--- TestVCF/Program.cs
+++ TestVCF/Program.cs
@@ -7,17 +7,21 @@ using Bio.VCF;
 
 namespace TestVCF
 {
-    class Program
-    {
-        static string fname = @"C:\Users\Nigel\SkyDrive\Bio.VCF\carolinensis-bwa-all-merged-trimmedreads-correctheader-freebayes.vcf";
-        static void Main(string[] args)
-        {
-            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
-            sw.Start();
-            VCFParser vcp = new VCFParser(fname);
-            var i=vcp.Select(x=>x.NoCallCount).Count();
-            sw.Stop();
-            Console.WriteLine(sw.Elapsed.ToString());
-        }
-    }
+	class Program
+	{
+		static string fname = @"C:\Users\Nigel\SkyDrive\Bio.VCF\carolinensis-bwa-all-merged-trimmedreads-correctheader-freebayes.vcf";
+
+		static void Main (string[] args)
+		{
+			fname = "testData/NA12878.knowledgebase.snapshot.20131119.b37.vcf.gz";
+			System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch ();
+			sw.Start ();
+			VCFParser vcp = new VCFParser (fname);
+			var j = vcp.First ();
+			var ii = j.Genotypes;
+			var i = vcp.Select (x => x.NoCallCount).Count ();
+			sw.Stop ();
+			Console.WriteLine (sw.Elapsed.ToString ());
+		}
+	}
 }
